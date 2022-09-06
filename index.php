41adea5b8ff1faac41f7f938268b7a7d42e5ff76
gukaguka <68656472+gukaguka@users.noreply.github.com>
2022-09-06 21:52:17 +0400

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,11 @@ header('Access-Control-Allow-Origin: *');
 
 header('Access-Control-Allow-Methods: GET, POST');
 
-header("Access-Control-Allow-Headers: X-Requested-With");
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
 
+header("HTTP/1.1 200 OK");
 
 echo "hello there";
 //Get Heroku ClearDB connection information
