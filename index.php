892091f32556d3f28ab9a1cb5680e34cfda64231
gukaguka <68656472+gukaguka@users.noreply.github.com>
2022-09-13 02:54:09 +0400

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
@@ -18,16 +19,11 @@ if ($conn->connect_error) {
 echo "Connected successfully";
 
 
-
-$sql = ' SELECT * FROM partner-locator ';
-
-    $myQuery = mysqli_query($conn, $sql);
+$sql = 'SELECT * FROM loc_country';
+$myQuery = mysqli_query($conn, $sql);
 
 
-    $result = mysqli_fetch_all($myQuery, MYSQLI_ASSOC);
+$result = mysqli_fetch_all($myQuery, MYSQLI_ASSOC);
 
-	print_r($result);
-
-
-
+print_r($result);
 ?>
